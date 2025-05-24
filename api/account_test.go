038193ddca82a0543514203d43ba5810b5d856f7
vihanga <vihaaanga.mihiranga@gmail.com>
2025-05-24 20:51:07 +1000

    Finish mock testing getAccount API

--- api/account_test.go
+++ api/account_test.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,31 +18,122 @@ import (
 )
 
 func TestGetAccountAPI(t *testing.T) {
+
+	//? Flow:
+	//? 1. create a fake account.
+	//? 2. configure fake db to send the appropriate response back when the api endpoint is called with the account id as parameter (setup an "expectation")
+	//? 3. start the server and send the get request for the account
+	//? 4. validate response content
 	account := randomAccount()
 
-	ctrl := gomock.NewController(t)
+	// define mock test case structure
+	type testCase struct {
+		name          string
+		accountID     int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}
 
-	// verify that all the expectations were met at the end of the test
-	defer ctrl.Finish()
+	testcases := []testCase{
+		{
+			name:      "OK",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				//? define expectations
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				// check response code
+				require.Equal(t, http.StatusOK, recorder.Code)
+				// check response body
+				requireBodyMatchAccount(t, recorder.Body, account)
+			},
+		},
+		{
+			name:      "NotFound",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				//? define expectation
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				// check response code
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+				// no account to check for content
 
-	store := mockdb.NewMockStore(ctrl)
+			},
+		},
+		{
+			name:      "InternalError",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				//? define expectation
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				// check response code
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+				// no account to check for content
 
-	// build mock
-	store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(account, nil)
+			},
+		},
+		{
+			name:      "BadRequest",
+			accountID: 0,
+			buildStubs: func(store *mockdb.MockStore) {
+				//? define expectation
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				// check response code
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+				// no account to check for content
 
-	// start test server and send request
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
+			},
+		},
+	}
 
-	url := fmt.Sprintf("/accounts/%d", account.ID)
+	//? loop through the test cases
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
 
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	require.NoError(t, err)
+			ctrl := gomock.NewController(t)
 
-	server.router.ServeHTTP(recorder, request)
+			// verify that all the expectations were met at the end of the test
+			defer ctrl.Finish()
 
-	// check response
-	require.Equal(t, http.StatusOK, recorder.Code)
+			store := mockdb.NewMockStore(ctrl)
+
+			// build stub
+			tc.buildStubs(store)
+
+			// start test server and send request to mock db
+			server := NewServer(store)
+
+			//! store the response from the server so we can validate its content later on
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/accounts/%d", tc.accountID)
+
+			// send the request for the account
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+
+			//make sure the server does not return an error
+			require.NoError(t, err)
+
+			// send back the response
+
+			//? 1. router receives the get request
+			//? 2. the getAccount handler function runs
+			//? 3. calls store.GetAccount(ctx, 123)
+			//? 4. mock responds by returning the fake account data as defined in the expectation
+			//? 5. the handler responses by sending JSON response back
+			//? 6. the recorder captures all response data (status, headers, body)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+
+		})
+	}
 
 }
 
@@ -54,4 +148,12 @@ func randomAccount() db.Account {
 
 func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
 
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+	var gotAccount db.Account
+
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+
+	require.Equal(t, account, gotAccount)
 }
